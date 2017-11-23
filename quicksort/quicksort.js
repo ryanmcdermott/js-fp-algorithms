20c11f5491d2d5a45b413257ae51254744dcd2dc
Ryan McDermott <ryantmcdermott@gmail.com>
2017-11-23 12:42:41 -0800

    Quicksort

---
+++ quicksort/quicksort.js
@@ -0,0 +1,42 @@
+function quicksort(sequence) {
+  return empty_p(sequence) ? [] : sort(first(sequence), rest(sequence));
+
+  function sort(x, xs) {
+    return concat(quicksort(left(x, xs)), [x], quicksort(right(x, xs)));
+  }
+
+  function left(x, xs) {
+    return xs.filter(function(val) {
+      return val <= x;
+    });
+  }
+
+  function right(x, xs) {
+    return xs.filter(function(val) {
+      return val > x;
+    });
+  }
+}
+
+// Helper functions
+function slice(sequence) {
+  return [].slice.call(sequence);
+}
+
+function empty_p(sequence) {
+  return !sequence.length;
+}
+
+function concat() {
+  return slice(arguments).reduce(function(acc, val) {
+    return acc.concat(val);
+  }, []);
+}
+
+function first(sequence) {
+  return sequence[0];
+}
+
+function rest(sequence) {
+  return sequence.slice(1);
+}
---
+++ run-length/run-length.js
@@ -0,0 +1,15 @@
+let list = ['a','a','a','a', 'b','b', 'c','c','c', 'd']
+
+let runLength = (l, result) => {
+  if (!l.length) return result;
+  return runLength(
+    _.dropWhile(l, item => item == l[0]),
+    result.concat(
+      l[0] + (
+        l.length - _.dropWhile(l, item => item == l[0]).length)
+      )
+  );
+}
+
+let result = runLength(list, [])
+console.log(result)
