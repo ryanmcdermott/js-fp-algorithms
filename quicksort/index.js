9218c578c28437d570adeb404173fa08933abf76
Ryan McDermott <ryantmcdermott@gmail.com>
2017-11-25 09:58:07 -0800

    clean up quicksort

--- quicksort/index.js
+++ quicksort/index.js
@@ -1,42 +1,10 @@
-export default function quicksort(sequence) {
-	return empty_p(sequence) ? [] : sort(first(sequence), rest(sequence));
+const quicksort = list => {
+	const left = (x, xs) => xs.filter(val => val <= x);
+	const right = (x, xs) => xs.filter(val => val > x);
+	const sort = (x, xs) =>
+		[].concat(quicksort(left(x, xs)), [x], quicksort(right(x, xs)));
 
-	function sort(x, xs) {
-		return concat(quicksort(left(x, xs)), [x], quicksort(right(x, xs)));
-	}
+	return !list.length ? [] : sort(list[0], list.slice(1));
+};
 
-	function left(x, xs) {
-		return xs.filter(function(val) {
-			return val <= x;
-		});
-	}
-
-	function right(x, xs) {
-		return xs.filter(function(val) {
-			return val > x;
-		});
-	}
-}
-
-// Helper functions
-function slice(sequence) {
-	return [].slice.call(sequence);
-}
-
-function empty_p(sequence) {
-	return !sequence.length;
-}
-
-function concat() {
-	return slice(arguments).reduce(function(acc, val) {
-		return acc.concat(val);
-	}, []);
-}
-
-function first(sequence) {
-	return sequence[0];
-}
-
-function rest(sequence) {
-	return sequence.slice(1);
-}
+export default quicksort;
