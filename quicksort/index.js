1a3ab689993e8033550a9fde3ea9bddf7830df49
Ryan McDermott <ryantmcdermott@gmail.com>
2017-11-25 09:45:42 -0800

    Fix up tests

--- quicksort/index.js
+++ quicksort/index.js
@@ -1,42 +1,42 @@
-function quicksort(sequence) {
-  return empty_p(sequence) ? [] : sort(first(sequence), rest(sequence));
+export default function quicksort(sequence) {
+	return empty_p(sequence) ? [] : sort(first(sequence), rest(sequence));
 
-  function sort(x, xs) {
-    return concat(quicksort(left(x, xs)), [x], quicksort(right(x, xs)));
-  }
+	function sort(x, xs) {
+		return concat(quicksort(left(x, xs)), [x], quicksort(right(x, xs)));
+	}
 
-  function left(x, xs) {
-    return xs.filter(function(val) {
-      return val <= x;
-    });
-  }
+	function left(x, xs) {
+		return xs.filter(function(val) {
+			return val <= x;
+		});
+	}
 
-  function right(x, xs) {
-    return xs.filter(function(val) {
-      return val > x;
-    });
-  }
+	function right(x, xs) {
+		return xs.filter(function(val) {
+			return val > x;
+		});
+	}
 }
 
 // Helper functions
 function slice(sequence) {
-  return [].slice.call(sequence);
+	return [].slice.call(sequence);
 }
 
 function empty_p(sequence) {
-  return !sequence.length;
+	return !sequence.length;
 }
 
 function concat() {
-  return slice(arguments).reduce(function(acc, val) {
-    return acc.concat(val);
-  }, []);
+	return slice(arguments).reduce(function(acc, val) {
+		return acc.concat(val);
+	}, []);
 }
 
 function first(sequence) {
-  return sequence[0];
+	return sequence[0];
 }
 
 function rest(sequence) {
-  return sequence.slice(1);
+	return sequence.slice(1);
 }
--- run-length/index.js
+++ run-length/index.js
@@ -1,15 +1,16 @@
-let list = ['a','a','a','a', 'b','b', 'c','c','c', 'd']
+import _ from 'lodash';
+
+let list = ['a', 'a', 'a', 'a', 'b', 'b', 'c', 'c', 'c', 'd'];
 
 let runLength = (l, result) => {
-  if (!l.length) return result;
-  return runLength(
-    _.dropWhile(l, item => item == l[0]),
-    result.concat(
-      l[0] + (
-        l.length - _.dropWhile(l, item => item == l[0]).length)
-      )
-  );
-}
+	if (!l.length) return result;
+	return runLength(
+		_.dropWhile(l, item => item == l[0]),
+		result.concat(
+			l[0] + (l.length - _.dropWhile(l, item => item == l[0]).length)
+		)
+	);
+};
 
-let result = runLength(list, [])
-console.log(result)
+let result = runLength(list, []);
+console.log(result);
