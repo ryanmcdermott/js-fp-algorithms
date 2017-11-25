ea3df95fcf52331066123aafd6c9b028d155fb93
Ryan McDermott <ryantmcdermott@gmail.com>
2017-11-24 18:22:16 -0800

    Fix test

--- test/binary-search.test.js
+++ test/binary-search.test.js
@@ -1,8 +1,8 @@
 import { expect } from 'chai';
 import binarySearch from '../binary-search';
 
-describe('test', function() {
-	it('binarySearch', function() {
+describe('binarySearch', function() {
+	it('shoudl find index of element of sorted array', function() {
 		const result = binarySearch(7, [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]);
 		expect(result).to.equal(3);
 	});
